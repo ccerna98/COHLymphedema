f0cacfe86f677fc9db36e7338ac11f199808d596
Celeste Cerna <ccerna@hmc.edu>
2020-04-02 10:06:05 -0700

    Fixed Formatting
    Improved formatting and add more to lymphedema page

---
+++ PatientApp/PatientApp/View Controllers/Lymphedema/LymphedemaMassagesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LymphedemaMassagesViewController.swift
+//  PatientApp
+//
+//  Created by Engineering on 3/2/20.
+//  Copyright © 2020 Darien Joso. All rights reserved.
+//
+
+import UIKit
+
+class LymphedemaMassagesViewController: UIViewController {
+    // put some stuff here eventually
+}
